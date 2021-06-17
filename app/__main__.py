c4e293f8610c9fac910e3517c5316f75c848f73f
diego-garro <diego.garromolina@yahoo.com>
2021-06-17 13:45:40 -0600

    Header UI complete without functionality

--- app/__main__.py
+++ app/__main__.py
@@ -1,33 +1,39 @@
 import tkinter as tk
+
+from .__colors__ import light_blue, white
 from .__version__ import version
 from .frames.header import Header
-from .__colors__ import white, light_blue
+
 
 class App(tk.Tk):
-    
     def __init__(self):
         super().__init__()
         self.title(f"AeroInformes - {version}")
         self.set_window_size_and_position()
-        self.geometry(f"{self.win_width}x{self.win_height}+{self.x_position}+{self.y_position}")
+        self.geometry(
+            f"{self.win_width}x{self.win_height}+{self.x_position}+{self.y_position}"
+        )
         self.resizable(False, False)
         self.config(bg=white)
-        
+
         # Set the app icon
-        icon = tk.PhotoImage(file='./app/assets/icons/plane.png')
-        self.tk.call('wm', 'iconphoto', self._w, icon)
-        
+        icon = tk.PhotoImage(file="./app/assets/icons/plane.png")
+        self.tk.call("wm", "iconphoto", self._w, icon)
+
         # Crete all frames
         self.header = Header(master=self, width=self.win_width, height=110)
-    
+
     def set_window_size_and_position(self):
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
-        
+
         self.win_width = round(screen_width * 0.40)
         self.win_height = round(screen_height * 0.80)
         self.x_position = screen_width // 2 - self.win_width // 2
-        self.y_position = screen_height // 2 - self.win_height // 2 - round(screen_height * 0.03)
+        self.y_position = (
+            screen_height // 2 - self.win_height // 2 - round(screen_height * 0.03)
+        )
+
 
 if __name__ == "__main__":
     root = App()
--- app/frames/header.py
+++ app/frames/header.py
@@ -1,10 +1,14 @@
-from tkinter import Frame, Label, Button
+from tkinter import Button, Frame, Label
+from tkinter import filedialog as fd
+
 from PIL import Image, ImageTk
-from ..__colors__ import white, light_blue
+
+from ..__colors__ import light_blue, white
+
+# TODO: Crear un método para extraer las imágenes desde los avisos de ceniza
+
 
 class Header(Frame):
-    
-    
     def __init__(self, master=None, width=100, height=100):
         super().__init__(master)
         self.master = master
@@ -13,32 +17,104 @@ class Header(Frame):
         self.pack()
         self.config(bg=white, width=self.width, height=self.height)
         self.create_widgets()
-    
-    
+
     def create_widgets(self):
         # set the imn logo
-        self.imn = ImageTk.PhotoImage(Image.open('./app/assets/img/logo_imn.png').resize(self._image_size(3.6), Image.ANTIALIAS))
+        self.imn = ImageTk.PhotoImage(
+            Image.open("./app/assets/img/logo_imn.png").resize(
+                self._image_size(3.6), Image.ANTIALIAS
+            )
+        )
         self.logo_imn = Label(self, image=self.imn, bd=0)
         self.logo_imn.grid(row=0, column=0, pady=10)
-        
+
+        # set space between images and buttons
         self.space = Label(self, width=4, bg=white)
         self.space.grid(row=0, column=1)
-        
-        # set the button to add .docx files
-        self.docx_button = Button(self, text="Agregar Mapa", fg=white, bg=light_blue, width=10, relief="flat")
-        self.docx_button.grid(row=0, column=2)
-        
+
+        # set the buttons to add SIGWX map and .docx files
+        self.buttons = self._create_buttons()
+        self.buttons.grid(row=0, column=2)
+
+        # set space between images and buttons
         self.space = Label(self, width=4, bg=white)
         self.space.grid(row=0, column=3)
-    
+
         # set the minae logo
-        self.minae = ImageTk.PhotoImage(Image.open('./app/assets/img/logo_minae.png').resize(self._image_size(3, height_cut=30), Image.ANTIALIAS))
+        self.minae = ImageTk.PhotoImage(
+            Image.open("./app/assets/img/logo_minae.png").resize(
+                self._image_size(3, height_cut=30), Image.ANTIALIAS
+            )
+        )
         self.logo_minae = Label(self, image=self.minae, bd=0, bg=white)
         self.logo_minae.grid(row=0, column=4, pady=10)
-    
-    
+
+    def _select_map(self):
+        filetypes = (
+            ("PNG images", ".png"),
+            ("JPG images", ".jpg"),
+            ("JPEG images", ".jpeg"),
+            ("GIF images", ".gif"),
+            ("All files", "."),
+        )
+
+        self.sigwx_map = fd.askopenfile(
+            initialdir="/Images",
+            title="Seleccione el Mapa SIGWX",
+            filetypes=filetypes,
+        )
+        # print(self.sigwx_map)
+
+    def _select_docx_files(self):
+        filetypes = (
+            ("MS Word files", ".docx"),
+            ("MS Word 97-2003 files", ".doc"),
+            ("All files", "."),
+        )
+
+        self.docx_files = fd.askopenfilenames(
+            initialdir="/Documents",
+            title="Seleccione los Avisos de Ceniza Volcánica",
+            filetypes=filetypes,
+        )
+
+        print(self.docx_files)
+
+    def _create_buttons(self):
+        __frame = Frame(self, bg=white)
+
+        # set button: add map
+        map_btn = Button(
+            __frame,
+            text="Agregar Mapa",
+            fg=white,
+            bg=light_blue,
+            width=10,
+            relief="flat",
+            command=self._select_map,
+        )
+        map_btn.grid(row=0, column=0)
+
+        # set space between buttons
+        space = Label(__frame, width=2, bg=white)
+        space.grid(row=1, column=0)
+
+        # set button: add docx
+        docx_btn = Button(
+            __frame,
+            text="Agregar .docx",
+            fg=white,
+            bg=light_blue,
+            width=10,
+            relief="flat",
+            command=self._select_docx_files,
+        )
+        docx_btn.grid(row=2, column=0)
+
+        return __frame
+
     def _image_size(self, factor, height_cut=0):
         width = self.width // factor
-        height = self.width // factor * 2/3 - height_cut
-        
+        height = self.width // factor * 2 / 3 - height_cut
+
         return round(width), round(height)
