1795c289e92a17715a6b11a3ad43e929acc1b234
diego-garro <diego.garromolina@yahoo.com>
2021-07-10 20:35:05 -0600

    Add logging to messagebox.py

--- app/frames/messagebox.py
+++ app/frames/messagebox.py
@@ -1,15 +1,22 @@
 from tkinter import messagebox as MessageBox
+from app.utils import logger
 
 
 def box(type: str, title: str, message: str):
     if type == "warning":
+        logger.info("Launching warning window.")
         MessageBox.showwarning(title, message)
     elif type == "error":
+        logger.info("Launching error window.")
         MessageBox.showerror(title, message)
     elif type == "okcancel":
+        logger.info("Launching okcancel window.")
         result = MessageBox.askokcancel(title, message)
         return result
     elif type == "showinfo":
+        logger.info("Launching showinfo window.")
         MessageBox.showinfo(title, message)
     else:
-        raise ValueError("invalid messagebox type.")
+        msg = "invalid messagebox type."
+        logger.error(msg.capitalize())
+        raise ValueError(msg)
